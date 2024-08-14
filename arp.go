0206988622985e9d6e7f66a0d261032aea993504
torbenconto <torbenmconto@gmail.com>
2024-08-14 00:12:47 -0400

    start tests and reply method

--- arp.go
+++ arp.go
@@ -12,6 +12,8 @@ import (
 	"github.com/torbenconto/arp-tools/internal/socket"
 )
 
+var BroadcastMac, _ = net.ParseMAC("ff:ff:ff:ff:ff:ff")
+
 type Packet struct {
 	EthernetHeader struct {
 		TargetMAC net.HardwareAddr
@@ -57,6 +59,26 @@ func NewPacket(
 	p.EthernetHeader.SourceMAC = sourceMAC
 
 	return &p
+}
+
+// Equals compares two Packet structs for equality
+func (p1 *Packet) Equals(p2 *Packet) bool {
+	if p1 == nil || p2 == nil {
+		return p1 == p2
+	}
+
+	return p1.EthernetHeader.TargetMAC.String() == p2.EthernetHeader.TargetMAC.String() &&
+		p1.EthernetHeader.SourceMAC.String() == p2.EthernetHeader.SourceMAC.String() &&
+		p1.EthernetHeader.EtherType == p2.EthernetHeader.EtherType &&
+		p1.HardwareType == p2.HardwareType &&
+		p1.ProtocolType == p2.ProtocolType &&
+		p1.HardwareSize == p2.HardwareSize &&
+		p1.ProtocolSize == p2.ProtocolSize &&
+		p1.Opcode == p2.Opcode &&
+		p1.TargetMAC.String() == p2.TargetMAC.String() &&
+		p1.TargetIP.String() == p2.TargetIP.String() &&
+		p1.SourceMAC.String() == p2.SourceMAC.String() &&
+		p1.SourceIP.String() == p2.SourceIP.String()
 }
 
 func (p *Packet) Unmarshal(data []byte) error {
@@ -149,7 +171,6 @@ func (a *Arp) Request(ip net.IP) error {
 		return err
 	}
 
-	broadcastMac, _ := net.ParseMAC("ff:ff:ff:ff:ff:ff")
 	packet := NewPacket(
 		ethernet.ARPEtherType,
 		ethernet.EthernetHardwareType,
@@ -157,7 +178,7 @@ func (a *Arp) Request(ip net.IP) error {
 		6,
 		4,
 		ethernet.SendOpcode,
-		broadcastMac,
+		BroadcastMac,
 		ip,
 		a.Socket.Intf().HardwareAddr,
 		addr,
@@ -213,4 +234,38 @@ func (a *Arp) Resolve(ip net.IP) (*Packet, error) {
 	}
 
 	return nil, fmt.Errorf("invalid ARP response or incorrect MAC address")
+}
+
+/*
+Reply sends an ARP reply to ip "sendTo" from ip "from" saying that "from" is at your interfaces MAC addr
+*/
+func (a *Arp) Reply(sendTo, from net.IP) error {
+	p, err := a.Resolve(sendTo)
+	if err != nil {
+		return err
+	}
+
+	packet := NewPacket(
+		ethernet.ARPEtherType,
+		ethernet.EthernetHardwareType,
+		ethernet.IPv4ProtocolType,
+		6,
+		4,
+		ethernet.RecvOpcode,
+		p.SourceMAC,
+		sendTo,
+		a.Socket.Intf().HardwareAddr,
+		from,
+	)
+
+	data, err := packet.Marshal()
+	if err != nil {
+		return err
+	}
+
+	if err := a.Socket.Write(data); err != nil {
+		return err
+	}
+
+	return nil
 }
---
+++ arp_packet_test.go
@@ -0,0 +1,50 @@
+package arptools
+
+import (
+	"crypto/rand"
+	"net"
+	"testing"
+
+	"github.com/torbenconto/arp-tools/cmd/ethernet"
+)
+
+func GenerateRandomMAC() net.HardwareAddr {
+	// Generate a random 6-byte MAC address.
+	mac := make([]byte, 6)
+	rand.Read(mac)
+
+	// Set the locally administered address bit (first byte should be 02).
+	mac[0] = (mac[0] & 0xFE) | 0x02
+
+	return net.HardwareAddr(mac)
+}
+
+func GenerateRandomIP() net.IP {
+	ip := make([]byte, 4)
+	rand.Read(ip)
+
+	return net.IP(ip)
+}
+
+func TestPacket_Marshal_Unmarshal(t *testing.T) {
+	// Create a packet
+	p1 := NewPacket(ethernet.ARPEtherType, ethernet.EthernetHardwareType, ethernet.IPv4ProtocolType, 6, 4, ethernet.SendOpcode, GenerateRandomMAC(), net.IPv4(10, 0, 0, 1), GenerateRandomMAC(), GenerateRandomIP())
+
+	// Marshal the packet
+	data, err := p1.Marshal()
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+
+	// Unmarshal the data into a new packet
+	var p2 Packet
+	err = p2.Unmarshal(data)
+	if err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+
+	// Compare packets
+	if !p1.Equals(&p2) {
+		t.Fatalf("Packets differ: %+v vs %+v", p1, p2)
+	}
+}
